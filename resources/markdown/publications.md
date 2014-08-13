445b15bca4c7932b3dc0ffd1d3419b94b9938fa1
louissmit <ghr1Co74G9>
2014-08-13 15:13:13 +0200

    project pages

---
+++ resources/markdown/publications.md
@@ -0,0 +1,10 @@
+Karen Ullrich, Jan Schlüter, and Thomas Grill: Boundary Detection in Music Structure Analysis using Convolutional Neural Networks. In Proceedings of the 15th International Society for Music Information Retrieval Conference (ISMIR 2014), Taipei, Taiwan, 2014. ([pdf](http://www.ofai.at/~jan.schlueter/pubs/2014_ismir.pdf),[Bibtex](
+
+@InProceedings{ullrich2014_ismir,
+	author = {Karen Ullrich and Jan Schl{\"u}ter and Thomas Grill},
+	title = {{Boundary Detection in Music Structure Analysis using Convolutional Neural Networks}},
+	year = {2014},
+	booktitle = {Proceedings of the 15th International Society for Music Information Retrieval Conference (ISMIR 2014)},
+	address = {Taipei, Taiwan},
+}
+))
--- src/k_server/core.clj
+++ src/k_server/core.clj
@@ -7,6 +7,7 @@
             [net.cgrand.enlive-html :as html]
             [hiccup.core :as hiccup]
             [clojure.data.json :as json]
+            [markdown.core :as md]
             [net.cgrand.reload :as reload]))
 
 
@@ -14,52 +15,94 @@
 (def data (json/read-str
            (slurp (clojure.java.io/resource "data.json")) :key-fn keyword))
 
-(defn render-for [el-list html-vec] render-for
+(defn get-md [path]
+  (md/md-to-html-string
+   (slurp
+    (clojure.java.io/resource
+     (str "markdown/" path)))))
+
+(defn render-for [el-list html-func]
   (apply str
-         (map #(-> (html-vec %) hiccup/html)
-                el-list)
-              )
-  )
+         (map (fn [[id project]]
+                (->
+                 (html-func id project)
+                 hiccup/html))
+                el-list)))
 
 (defn project-thumbs [projects-list]
   (render-for projects-list
-              (fn [project]
-                [:div {:class "project-thumb" :display "none"}
+              (fn [id project]
+                [:div {:class "project-thumb"}
                   [:h4 {:class "title"} (:title project)]
                   [:a
-                   {:href (str "projects/"(:id project))}
+                   {:href (str "projects/" (name id))}
                    [:img
-                    {:src (str "img/projects/" (:image_name project))
+                    {:src (str "/img/projects/" (:image_name project))
                      :class "img-responsive"}]
                    ]
                 ])))
 
+(project-thumbs (:projects data))
+
+(map (fn [[k v]] (pr v)) (:projects data))
+
 (defn get-nav [sections]
-  (render-for sections
-              (fn [section]
-                [:li [:a
-                      {:href (str "#" section)}
-                      section]])))
+  (apply str
+  (map (fn [section]
+         (hiccup/html
+            [:li [:a
+               {:href (str "#" section)}
+               section]])) sections)))
+
+
+(defn get-contact [contact]
+      (let [{:keys [name address phone emails]} contact]
+        (hiccup/html
+        [:div
+          [:span name]
+          [:address
+           [:span (:name address)]
+           [:br]
+           [:span (:street address)]
+           [:br]
+           [:span (:postal address)]
+           [:span (:city address)]]
+          [:span phone]
+          [:br]
+           (let [email (emails 1)]
+            [:a {:href (str "mailto:" email)} email])
+          ])))
 
 (html/deftemplate base-template "templates/index.html"
   []
-  [:head :title] (html/content "karen ullrich")
+  [:ul.navbar-nav] (html/html-content (get-nav sections))
+  [:#intro] (html/html-content (get-md "about.md"))
   [:#projects] (html/clone-for [section sections]
                            [:h1] (html/content section)
                            [:.section](html/set-attr :id section))
   [:#projects :.content] (html/html-content (project-thumbs (:projects data)))
-  [:ul.navbar-nav] (html/html-content (get-nav sections)))
+  [:#publications :.content] (html/html-content (get-md "publications.md"))
+  [:#contact :.content] (html/html-content (get-contact (:contact data))))
 
-
+(html/deftemplate project-template "templates/index.html"
+  [id]
+  [:.logo] (html/set-attr :href "/")
+  [:#main] (html/html-content (hiccup/html
+                               [:div (get-md (get-in data [:projects (keyword id) :markdown]))])))
 
 (defresource main
   ;; main resource
   :available-media-types ["text/html"]
   :handle-ok (fn [_] (apply str (base-template))))
 
+(defresource project [req]
+  ;; main resource
+  :available-media-types ["text/html"]
+  :handle-ok (fn [_] (apply str (project-template (get-in req [:route-params :id])))))
+
 (defroutes app
   (ANY "/" [] main)
-  (ANY "/projects/" [] main)
+  (ANY "/projects/:id" [req] project)
   (route/resources "/"))
 
 (def handler
