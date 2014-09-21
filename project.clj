cfcf505d656f62be9be3e75cef1a40310c5f9d38
louissmit <ghr1Co74G9>
2014-09-21 17:51:29 +0200

    ring update

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [liberator "0.10.0"]
                  [compojure "1.1.3"]
-		             [ring/ring-core "1.2.1"]
+		             [ring/ring-core "1.3.1"]
                  [enlive "1.1.5"]
                  [markdown-clj "0.9.47"]
-                 [ring/ring-jetty-adapter "1.1.0"]
+                 [ring/ring-jetty-adapter "1.3.1"]
                  [prone "0.6.0"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler k-server.core/handler
